402d273c55daa1dfbd129f3676d957ae57e9db14
luke-schleicher <luke.j.schleicher@gmail.com>
2017-01-03 10:42:47 -0600

    initial push to anthony

--- scripts.js
+++ scripts.js
@@ -1,16 +1,46 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
-    // your code here
+  largestEl: function(array){
+    var largest = array[0];  
+    for (var i = 1; i < array.length; i++) {
+      if (array[i] > largest) {
+        largest = array[i];
+      }
+    }
+    return largest;
   },
   
-  reversed: function(){  
-    // your code here
+  reversed: function(string){  
+    var reversed = new Array(string.length);
+    var characters = string.split('');
+    for (var i = 0; i < characters.length; i++) {
+      reversed[i] = characters[characters.length - 1 - i];
+    }
+    return reversed.join('');
   },
 
-  loudSnakeCase: function(){  
-    // your code here
+  loudSnakeCase: function(string){  
+    var snakeCase = [];
+    var characters = string.split('');
+    var punctuation = ['.', '!'];
+    for (var i = 0; i < characters.length; i++) {
+      // if first letter, capitalize
+      // if punctuation, remove
+      // if space, remove and replace with underscore
+      if (characters[i] === ' ') {
+        if (snakeCase[i - 1] !== '_') {
+          snakeCase[i] = '_';
+        }
+      } else if (!punctuation.includes(characters[i])) {
+        if (snakeCase[i - 1] === '_') {
+          snakeCase[i] = characters[i].toUpperCase();
+        } else {
+          snakeCase[i] = characters[i];
+        }
+      }
+    }
+    return snakeCase.join('');
   },
 
   compareArrays: function(){ 
