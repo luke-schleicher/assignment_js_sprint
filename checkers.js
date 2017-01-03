bcb8a513ebcb4232cf891c7fb3258802197fe010
luke-schleicher <luke.j.schleicher@gmail.com>
2017-01-03 17:35:45 -0600

    checkers

--- checkers.js
+++ checkers.js
@@ -7,12 +7,23 @@ function Board(height, width) {
               new Array(8), new Array(8),
               new Array(8), new Array(8),
               new Array(8), new Array(8)];
-
-  this.squareContents = function(xCoordinate, yCoordinate) {
-    return this.grid[xCoordinate][yCoordinate];
-  };
-
 }
+
+var bProto = Board.prototype;
+
+bProto.squareContents = function(xCoordinate, yCoordinate) {
+  return this.grid[xCoordinate][yCoordinate];
+};
+
+bProto.rows = function() {
+  var rows = new Array(this.height);
+  for (var rowNumber = 0; rowNumber < rows.length; rowNumber++) {
+    var row = new Array(this.width);
+    for (var colNumber = 0; colNumber < row.length; colNumber++) {
+      this.grid[colNumber][rowNumber];
+    }
+  }
+};
 
 function Piece(playerNumber, symbol) {
   this.playerNumber = playerNumber;
@@ -22,7 +33,21 @@ function Piece(playerNumber, symbol) {
 function Checkers() {
   this.board = new Board(8, 8);
   this.setBoard = function(setTopPieces, setBottomPieces) {
-    setTopPieces();
-    setBottomPieces();
-  }
+    this.setTopPieces();
+    this.setBottomPieces();
+  };
+
 }
+
+var cProto = Checkers.prototype;
+
+cProto.setTopPieces = function() {
+
+  //
+
+  var rowOffset = rowNumber % 2 === 0 ? 0 : 1;
+
+}
+
+
+
